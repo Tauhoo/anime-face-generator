c3ad465f13b378f599bbeecc700eff8ec84c8694
wachirawit wacharak <tauhoo@wachirawits-MacBook-Pro.local>
2020-04-16 15:28:21 +0700

    set up discriminator

---
+++ src/discriminator.py
@@ -0,0 +1,27 @@
+import tensorflow.keras as keras
+import tensorflow.nn.layers as layers
+
+
+class discriminator:
+    def __init__(self, height, width, channels):
+        self.input = layers.Input(shape=(height, width, channels))
+        self.model = layers.Conv2D(128, 3)(self.input)
+        self.model = layers.LeakyReLU(0.2)(self.model)
+        self.model = layers.Conv2D(128, 4, strides=2)(self.model)
+        self.model = layers.LeakyReLU(0.2)(self.model)
+        self.model = layers.Conv2D(128, 4, strides=2)(self.model)
+        self.model = layers.LeakyReLU()(self.model)
+        self.model = layers.Conv2D(128, 4, strides=2)(self.model)
+        self.model = layers.LeakyReLU()(self.model)
+
+        self.model = layers.Flatten()(self.model)
+
+        self.model = layers.Dropout(0.4)(self.model)
+
+        self.model = layers.Dense(1, activation='sigmoid')(self.model)
+
+        self.model = keras.models.Model(self.input, self.model)
+
+    def summary(self):
+        self.model.summary()
+
